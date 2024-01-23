0209f1c0bf009b6355fd69867603aa4b2807d414
Dammymure <dammymure1@gmail.com>
2024-01-23 10:55:50 +0100

    working on the front end of this web app

---
+++ client/src/components/carmatch/index.jsx
@@ -0,0 +1,116 @@
+import React from 'react'
+// import CarBuying from "../../assets/carLogos/buying-power.png";
+import Carbuying from "../../assets/designImages/buying-power.png";
+import ArrowDown from "../../assets/icons/arrowDown.png"
+
+
+function CarMatch() {
+  return (
+    <section>
+      <div className="flex flex-col sm:flex sm:flex-row  mx-auto w-[95%]">
+        <div className=" sm:w-[50%] flex flex-col sm:block mx-auto">
+          <p className="bg-blue-300 inline-block text-xs p-2 rounded font-medium">
+            NEW
+          </p>
+          <h1 className="text-3xl font-medium text-slate-800">
+            Know your buying power
+          </h1>
+          <p className="text-[16px] font-medium text-slate-500">
+            How much car can you afford? Find out, then see vehicles that match
+            your budget
+          </p>
+
+          <div className="flex mx-auto w-[100%]">
+            <img className="w-[100%]" src={Carbuying} alt="" />
+          </div>
+        </div>
+        <div className="sm:w-[50%] p-12">
+          <div className="text-center">
+            <h1 className="text-4xl font-medium">1,500,000</h1>
+            <p className="font-medium">Est. buying power</p>
+            <p className="font-medium text-slate-500">Based on 8.68% APR</p>
+          </div>
+          <div className="">
+            <div className="flex justify-between gap-4 py-5">
+              <div className="relative bg-white pt- flex items-center rounded-[2px] w-[100%] border border-slate-600">
+                <div className="relative rounded w-[100%]">
+                  <label className="absolute top-2 left-0 pl-1 text-xs text-slate-600 whitespace-nowrap font-medium">
+                    Looking For
+                  </label>
+                  <select
+                    className="appearance-none pt-6 pl-1 active:ring-white rounded w-[100%] font-medium"
+                    required=""
+                    aria-label="Looking for"
+                    // class="bg-red-400"
+                  >
+                    <option value="NEW">New car</option>
+                    <option value="USED">Used car</option>
+                  </select>
+                </div>
+                <div className="absolute right-0 pr-1">
+                  <img className="w-4" src={ArrowDown} />
+                </div>
+              </div>
+              <div className=" bg-white pt- flex items-center rounded-[2px] w-[100%] border border-slate-600">
+                <div className="relative rounded w-[100%]">
+                  <label className="absolute top-2 left-0 pl-1 text-xs text-slate-600 font-medium">
+                    Down payment
+                  </label>
+                  <input
+                    className=" pt-6 pl-1 active:ring-white rounded w-[100%] font-medium"
+                    type="text"
+                    aria-label="Looking for"
+                    value="3,000"
+                  ></input>
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-between gap-4">
+              <div className="relative bg-white pt- flex items-center rounded-[2px] w-[100%] border border-slate-600">
+                <div className="relative rounded w-[100%]">
+                  <label className="absolute top-2 left-0 pl-1 text-xs text-slate-600 font-medium">
+                    Credit score
+                  </label>
+                  <select
+                    className="appearance-none pt-6 pl-1 active:ring-white rounded w-[100%] font-medium"
+                    required=""
+                    aria-label="Looking for"
+                    // class="bg-red-400"
+                  >
+                    <option value="NEW">500,000 - 1,500,000</option>
+                    <option value="USED">1,500,500 - 3,500,000</option>
+                    <option value="USED">3,500,000 - 5,500,000</option>
+                    <option value="USED">5,500,000 - 8,500,000</option>
+                  </select>
+                </div>
+                <div className="absolute right-0 pr-1">
+                  <img className="w-4" src={ArrowDown} />
+                </div>
+              </div>
+              <div className=" bg-white pt- flex items-center rounded-[2px] w-[100%] border border-slate-600">
+                <div className="relative rounded w-[100%]">
+                  <label className="absolute top-2 left-0 pl-1 text-xs text-slate-600 whitespace-nowrap font-medium font-medium">
+                    Monthly payment
+                  </label>
+                  <input
+                    className=" pt-6 pl-1 active:ring-white rounded w-[100%] font-medium"
+                    type="text"
+                    aria-label="Looking for"
+                    value="250,000"
+                  ></input>
+                </div>
+              </div>
+            </div>
+            <div className="py-10">
+              <button className="w-[100%] text-white font-medium bg-black rounded-full py-2">
+                See your matches
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default CarMatch
--- client/src/scenes/Home/index.jsx
+++ client/src/scenes/Home/index.jsx
@@ -2,24 +2,39 @@ import React, { useState } from "react";
 import Navbar from "../../components/navbar";
 import CountUp from "react-countup";
 import ScrollTrigger from "react-scroll-trigger";
+import benz from "../../assets/carLogos/benz.png"
+import honda from "../../assets/carLogos/honda.png"
+import audi from "../../assets/carLogos/audi.png"
+import bentley from "../../assets/carLogos/bentley.png"
+import bmw from "../../assets/carLogos/bmw.png"
+import ferrari from "../../assets/carLogos/ferrari.png"
+import tesla from "../../assets/carLogos/tesla-logo.png"
+import lexus from "../../assets/carLogos/lexus.png"
+import jeep from "../../assets/carLogos/jeep-logo.png"
+import rolls from "../../assets/carLogos/rollsroyce.png"
+import lambo from "../../assets/carLogos/lambo.png"
+import hyundai from "../../assets/carLogos/hyundai.png";
+import porsche from "../../assets/carLogos/porsche.png";
+import CarMatch from "../../components/carmatch";
+
 
 function Home() {
-  const [counterOn, setCounterOn] = useState(false)
+  const [counterOn, setCounterOn] = useState(false);
   return (
     <div className="bg-white h-screen">
       <Navbar />
 
-      <section>
-        <div className="flex ">
-          <div className="sm:w-[50%] h-screen w-full flex flex-col text-center items-center justify-center px-10">
-            <h2 className="text-4xl font-semibold">
+      <section className="overflow-x-hidden">
+        <div className="flex flex-col-reverse sm:flex-row ">
+          <div className="sm:w-[50%] h-screen w-full flex flex-col text-center items-center justify-start sm:justify-center px-10 py-10">
+            <h2 className="text-2xl sm:text-4xl font-bold">
               Navigate Your Next Adventure:
               <ScrollTrigger
-                style={{ display: "inline-flex"}}
+                style={{ display: "inline-flex" }}
                 onEnter={() => setCounterOn(true)}
                 onExit={() => setCounterOn(false)}
               >
-                <span className="text-5xl font-bold">
+                <span className="text-3xl sm:text-5xl font-bold">
                   {counterOn && (
                     <CountUp start={20} end={50000} duration={2} delay={0} />
                   )}
@@ -28,21 +43,101 @@ function Home() {
               </ScrollTrigger>
               Cars, Infinite Possibilities.
             </h2>
-            <span>And Counting*</span>
-            <p>
+            {/* <span>And Counting*</span> */}
+            <p className="text-xs sm:text-sm ">
               Embark on a journey where every digit counts. Choose from a
-              staggering 40,000+ cars on [Your App Name], where buyers find
+              staggering 50,000+ cars on [Your App Name], where buyers find
               their dream ride, and sellers connect with a vast audience. Your
               next adventure starts with the power of numbers.
             </p>
+            <div className="flex gap-3 pt-5">
+              <button className="bg-black text-white text-sm font-semibold px-4 py-2 rounded-full">
+                Sign Up
+              </button>
+              <button className="bg-[#FE5F00] text-white text-sm font-semibold px-3 py-1 rounded-full">
+                Buy Now
+              </button>
+            </div>
+
+            <div class="slider pt-10">
+              <div class="slide-track">
+                <img src={lexus} className="h-10 sm:h-20" alt="h-" />
+                <img src={benz} className="h-10 sm:h-20" alt="" />
+                <img src={ferrari} className="h-10 sm:h-20" alt="" />
+                <img src={tesla} className="h-10 sm:h-20" alt="" />
+                <img src={honda} className="h-10 sm:h-20" alt="" />
+                <img src={bentley} className="h-10 sm:h-20" alt="" />
+                <img src={bmw} className="h-10 sm:h-20" alt="" />
+                <img src={lambo} className="h-10 sm:h-20" alt="" />
+                <img src={jeep} className="h-10 sm:h-20" alt="" />
+                <img src={rolls} className="h-10 sm:h-20" alt="" />
+                <img src={hyundai} className="h-10 sm:h-20" alt="" />
+                <img src={audi} className="h-10 sm:h-20" alt="" />
+                <img src={bentley} className="h-10 sm:h-20" alt="" />
+                {/* <img src={jeep} alt="" /> */}
+                <img src={ferrari} className="h-10 sm:h-20" alt="" />
+                <img src={bmw} className="h-10 sm:h-20" alt="" />
+                <img src={porsche} className="h-10 sm:h-20" alt="" />
+                {/* <img src={lexus} alt="" /> */}
+              </div>
+
+              {/* <div class="logos-slide ">
+                <img src={rolls} alt="" />
+                <img src={hyundai} alt="" />
+                <img src={audi} alt="" />
+                <img src={bentley} alt="" />
+                <img src={ferrari} alt="" />
+                <img src={jeep} alt="" />
+                <img src={bmw} alt="" />
+                <img src={porsche} alt="" />
+                <img src={lexus} alt="" />
+              </div> */}
+            </div>
           </div>
-          <div className="w-[50%] flex heroimage text-center items-center justify-center">
-            <h1 className="text-6xl text-white font-bold">
-              Buy and Seller you Cars in 3 Easy Steps
-            </h1>
+          <div className="py-10 sm:w-[50%] flex heroimage text-center items-center justify-center">
+            <div>
+              <h1 className="text-3xl px-[2vw] sm:text-6xl text-white font-bold">
+                Buy and Sell you Cars in 3 Easy Steps
+              </h1>
+              <div class="bg-white my-5 py-1 px-4 rounded-full shadow-md flex items-center mx-[7vw]">
+                {/* <!-- Search Icon --> */}
+                <svg
+                  class="w-6 h-6 text-gray-500 mr-2"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M21 21l-4.35-4.35M15 10a7 7 0 11-14 0 7 7 0 0114 0z"
+                  ></path>
+                </svg>
+                {/* <!-- Search Input --> */}
+                <input
+                  type="text"
+                  placeholder="Search make, year, model ..."
+                  class="border-none outline-none focus:ring focus:ring-orange-500/90 py-2 px-4 rounded-md w-40 sm:w-60 md:w-80 lg:w-96 xl:w-120 "
+                />
+              </div>
+              <div className="flex justify-center gap-3 py-8 text-white font-semibold text-lg">
+                <h1 className="underline cursor-pointer">Shop New</h1>
+                <h1 className="underline cursor-pointer">Shop Used</h1>
+              </div>
+            </div>
           </div>
         </div>
       </section>
+
+      {/*  */}
+      {/* <section className="relative">
+        <div className="absolute bg-black h-8 rotate-[5deg] md:rotate-[1deg] w-screen"></div>
+        <div className="absolute bg-[#FE5F00] h-8 rotate-[-5deg] md:rotate-[-1deg] w-screen"></div>
+        <div></div>
+      </section> */}
+      <CarMatch/>
     </div>
   );
 }
